63b9fcee16dba3d914b67642a39f0de87bce90c0
Tiago Ramirez <tiagoramirez2001@gmail.com>
2022-06-27 15:09:05 -0300

    Agregado boton de return

--- src/app/about-me/about-me.component.ts
+++ src/app/about-me/about-me.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
   templateUrl: './about-me.component.html',
   styleUrls: ['./about-me.component.css']
 })
-export class AboutMeComponent implements OnInit {
+export class AboutMeComponent {
 
-  constructor() { }
+  constructor(private router:Router){
 
-  ngOnInit(): void {
+  }
+
+  goHome(){
+    this.router.navigate(['']);
   }
 
 }
